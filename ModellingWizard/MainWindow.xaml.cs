1def177ae3db27b959079717675570d30e243606
Michael Grote <93448865+michi3214@users.noreply.github.com>
2023-03-10 12:24:26 +0100

    Add Community Toolkit for WinUI3

--- ModellingWizard/MainWindow.xaml.cs
+++ ModellingWizard/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using CommunityToolkit.WinUI.UI.Controls;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
