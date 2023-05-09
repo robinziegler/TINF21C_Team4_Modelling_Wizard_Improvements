4cca9863af491960dd7f988e5ed494b405d1bcf6
Max Trumpp <96117277+maximiliantrumpp@users.noreply.github.com>
2023-05-09 18:58:28 +0200

    Added About and Manual

--- ModellingWizard/UIs/ModalViews/Help_Menu/AboutPage.xaml.cs
+++ ModellingWizard/UIs/ModalViews/Help_Menu/AboutPage.xaml.cs
@@ -8,11 +8,13 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using ModellingWizard.Objects;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -31,6 +33,30 @@ namespace ModellingWizard.UIs.ModalViews.Help_Menu
             this.InitializeComponent();
             this.Height = 500;
             this.Width = 500;
+            
+        }
+
+        private void AboutText_Loaded(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new();
+            sb.AppendLine("This Modelling Wizard for Devices standalone application can be used to create or modify Devices and Interfaces. It can als be used to import IODD and GSDML Files whih will be converted to an AMLX Package. Also added functionality to convert EDZ files." + Environment.NewLine);
+            sb.AppendLine("Version: " + Instances.AppVersion + Environment.NewLine );
+            sb.AppendLine("Build: " + Instances.Build + Environment.NewLine );
+            sb.AppendLine("Design and Software development of earlier version by:" + Environment.NewLine);
+            sb.AppendLine("TINF17C (DHBW Stuttgart)" + Environment.NewLine) ;
+            sb.AppendLine("Raj Kumar Pulaparthi (Otto-von-Guericke University Magdeburg)" + Environment.NewLine) ;
+            sb.AppendLine("TINF20C (DHBW Stuttgart)" + Environment.NewLine+ Environment.NewLine) ;
+            sb.AppendLine("Design and Software development of current version by" + Environment.NewLine) ;
+            sb.AppendLine("TINF21C (DHBW Stuttgart)" + Environment.NewLine + Environment.NewLine) ;
+            sb.AppendLine("This Pulgin was created as a group project in the class \"Software Engineering\"" + Environment.NewLine) ;
+            sb.AppendLine("Later this plugin was developed as a sample tool that create vendor independant automaton component and inclued as part of Master Thesis" + Environment.NewLine) ;
+            sb.AppendLine("Terms and conditions for copying, distribution and modification:" + Environment.NewLine) ;
+            sb.AppendLine("This project is licensed under the GPL 3.0 license." + Environment.NewLine) ;
+            sb.AppendLine("Please visit out GitHub-Repository to learn more about this." + Environment.NewLine) ;
+            sb.AppendLine("This Plugin uses third-party software for the convertions of IODD and GSDML." + Environment.NewLine) ;
+            sb.AppendLine("All Rights reserved by the corresponding copyright owner." + Environment.NewLine) ;
+
+            AboutText.Text = sb.ToString();
         }
     }
 }
